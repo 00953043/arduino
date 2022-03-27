11c878607330df4432753211db037a3d285dfd5f
00953043 <100845205+00953043@users.noreply.github.com>
2022-03-27 22:42:48 +0800

    Add files via upload

--- UART_read.ino.ino
+++ UART_read.ino.ino
@@ -1,13 +1,18 @@
+int incomingByte = 0; // for incoming serial data
+
 void setup() {
-  Serial.begin(9600); 
-   Serial1.begin(9600);
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  Serial.println("Serial port begin at baud=9600...");
 }
 
 void loop() {
-  if (Serial.available()) {
-   Serial.write(Serial.read());
-  }
-   if(Serial.available()){
-    Serial.write(Serial.read());
+  // reply only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    // show what you got:
+    Serial.print("Received: ");
+    Serial.println(incomingByte, HEX);
   }
 }
